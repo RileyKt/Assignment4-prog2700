9d9ddb6866b955aa17db4358a36ddb82169155f3
Riley Kneen-Teed <kneenrt@gmail.com>
2023-11-21 03:21:30 -0400

    working on puzzle logic

--- main.js
+++ main.js
@@ -7,16 +7,19 @@
     .then((json) => {
         console.log(json);
         createGrid(json);
-        
+        createStatusCheckButton();
     })
     .catch(error =>{
         console.error("Error fetching json data: ", error);
     });
+
+    //function to create grid structure using the DOM, creates a div element for each row and cell.
     function createGrid(data) {
         const gridContainer = document.createElement('div');
         gridContainer.className = 'grid';
 
 
+        //for each to create the cells, and add event listeners, to change the color of the cell based on being clicked 
         data.rows.forEach((row, rowIndex) => {
             const rowDiv = document.createElement('div');
             rowDiv.className = 'row';
@@ -29,17 +32,29 @@
 
             gridContainer.appendChild(rowDiv);
         });
-      
+        
         document.body.appendChild(gridContainer);
-      }
-      
-      function toggleCellState(cell, cellDiv) {
+    }
+    
+    //function for toggling the state of a cell, only if its not colored from the start.
+    function toggleCellState(cell, cellDiv) {
+        if (!cell.canToggle) return;
         cell.currentState = (cell.currentState + 1) % 3;
 
         cellDiv.className = `cell state-${cell.currentState}`;
     }
 
-    
+    //function to create button to check puzzle status
+    function createStatusCheckButton() {
+        const button = document.createElement('button');
+        button.textContent = 'Check Puzzle Status';
+        button.addEventListener('click', checkPuzzleStatus);
+        document.body.appendChild(button);
+    }
+
+    function checkPuzzleStatus() {
+        return;
+    }
 
 
 
